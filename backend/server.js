ced27eb8c3205e54feeb3306a645208e7f78f717
Aastha <aastha.2428cseai1398@kiet.edu>
2026-04-22 18:12:28 +0530

    fix express wildcard routing for render

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.use("/api", require("./routes/authRoutes"));
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // React routing fallback
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
